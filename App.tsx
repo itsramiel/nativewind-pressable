6d57dd4e092fdf525af40af4f79b5f5336641dc9
ramiel <ramielwan48@gmail.com>
2022-11-17 22:21:58 +0200

    repro done

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center">
+      <Pressable
+        style={({ pressed }) => ({
+          opacity: pressed ? 0.2 : 1,
+        })}
+        className="bg-red-400"
+      >
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
